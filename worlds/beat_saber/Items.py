62636385e5aeb7a7762f29c711084721ec1f790b
N00byKing <N00byKing@hotmail.de>
2025-02-19 00:19:26 +0100

    beat_saber: New world

---
+++ worlds/beat_saber/Items.py
@@ -0,0 +1,17 @@
+from BaseClasses import Item
+
+class BSItem(Item):
+    game: str = "Beat Saber"
+
+item_table = {
+    "Song 01": 0,
+    "Song 02": 1,
+    "Song 03": 2,
+    "Song 04": 3,
+    "Song 05": 4,
+    "Song 06": 5,
+    "Song 07": 6,
+    "Song 08": 7,
+    "Song 09": 8,
+    "Song 10": 9,
+}
