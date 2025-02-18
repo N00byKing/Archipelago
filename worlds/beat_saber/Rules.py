39d340668c367f20bd73082faf460087565d01c8
N00byKing <N00byKing@hotmail.de>
2025-02-19 00:19:26 +0100

    beat_saber: New world

---
+++ worlds/beat_saber/Rules.py
@@ -0,0 +1,5 @@
+import typing
+from ..generic.Rules import add_rule
+
+def set_rules(multiworld, options, player, area_connections: typing.Dict[int, int], area_cost_map: typing.Dict[int, int]):
+    pass
