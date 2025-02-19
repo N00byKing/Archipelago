d77f36515c9f80ebda6d4779673719f5351a98a3
N00byKing <N00byKing@hotmail.de>
2025-02-19 23:29:54 +0100

    Generate custom campaign

--- worlds/beat_saber/Options.py
+++ worlds/beat_saber/Options.py
@@ -2,6 +2,14 @@ import typing
 from dataclasses import dataclass
 from Options import Option, DeathLink, Range, Toggle, PerGameCommonOptions
 
+class NumTracks(Range):
+    """Amount of Tracks to require"""
+    display_name = "Number of Tracks"
+    range_start = 3
+    range_end = 50
+    default = 3
+
 @dataclass
 class BSOptions(PerGameCommonOptions):
+    num_tracks: NumTracks
     death_link: DeathLink
