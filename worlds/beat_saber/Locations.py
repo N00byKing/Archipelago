0e41dbc24ce361b00c6f08cb1fc9555ac794d454
N00byKing <N00byKing@hotmail.de>
2025-02-19 23:29:54 +0100

    Generate custom campaign

--- worlds/beat_saber/Locations.py
+++ worlds/beat_saber/Locations.py
@@ -3,15 +3,4 @@ from BaseClasses import Location
 class BSLocation(Location):
     game: str = "Beat Saber"
 
-location_table = { # Key is BeatSaver mapid
-    "[Extra Sensory II] Xtrullor - Ego Death": 0x43A2E,
-    "[Extra Sensory II] RXLZQ - Through The Screen": 0x43A5D,
-    "[Extra Sensory II] Simplifi - you": 0x43A1F,
-    "Track 04": 3,
-    "Track 05": 4,
-    "Track 06": 5,
-    "Track 07": 6,
-    "Track 08": 7,
-    "Track 09": 8,
-    "Track 10": 9,
-}
+location_table = { "Track " + f"{i}".zfill(2) : i for i in range(50) }
