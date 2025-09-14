0bc92af66407b6af2d6fd42aa2749d4c58683c66
N00byKing <N00byKing@hotmail.de>
2025-09-14 18:18:10 +0200

    Remove bs id offset

--- worlds/beat_saber/Locations.py
+++ worlds/beat_saber/Locations.py
@@ -3,5 +3,4 @@ from BaseClasses import Location
 class BSLocation(Location):
     game: str = "Beat Saber"
 
-bs_id_offset = 140400
-location_table = { "Node " + f"{i}".zfill(2) : bs_id_offset + i for i in range(50) }
+location_table = { "Node " + f"{i}".zfill(2) : i for i in range(50) }
